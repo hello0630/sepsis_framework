88ddddc9242e5ed2f7d070c570398acb47fdef1c
James Morrill <james.morrill.6@gmail.com>
2019-12-05 17:10:34 +0000

    Forward fill method.

--- src/data/transformers.py
+++ src/data/transformers.py
@@ -87,6 +87,37 @@ class DerivedFeaturesTorch():
         return dataset
 
 
+class ForwardFill():
+    """ Forward fill for a torch tensor.
+
+    This (currently) assumes a torch tensor input of shape [N, L, C] and will forward will along the 2nd (L)
+    dimension.
+
+    https://stackoverflow.com/questions/41190852/most-efficient-way-to-forward-fill-nan-values-in-numpy-array
+    """
+
+    @staticmethod
+    def ffill2d(arr):
+        """ 2d ffill. """
+        mask = np.isnan(arr)
+        idx = np.where(~mask, np.arange(mask.shape[1]), 0)
+        np.maximum.accumulate(idx, axis=1, out=idx)
+        out = arr[np.arange(idx.shape[0])[:, None], idx]
+        return out
+
+    def transform(self, data):
+        data_ffilled = torch.Tensor([self.ffill2d(x.numpy().T) for x in data]).transpose(1, 2)
+        return data_ffilled
+
+
 if __name__ == '__main__':
     dataset = load_pickle(DATA_DIR + '/interim/from_raw/sepsis_dataset.dill', use_dill=True)
+
+    # Perform forward fill
+    ffill = ForwardFill().transform(dataset.data)
+    dataset.update_data(ffill)
+
+    # Derive features
     dataset = DerivedFeaturesTorch().transform(dataset)
+
+
